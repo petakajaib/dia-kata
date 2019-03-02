2c0fe1a3aa6aad2d8542d42d3e9c233a469e12a0
Amir Othman <th3go2@gmail.com>
2019-03-02 16:46:29 +0800

    debug

--- vectorization.py
+++ vectorization.py
@@ -63,11 +63,51 @@ def get_distance_of_entity_to_quote_vector(entry):
 
     return vector
 
+def get_token_count(cleaned_content, token):
+
+    token = str(token).lower()
+
+    parsed = Text(cleaned_content.lower())
+
+    content_tokens = [str(t) for t in parsed.tokens]
+
+    count = 0
+
+    for content_token in content_tokens:
+
+        if content_token == token:
+            count  += 1
+
+    return count
+
+
+def get_frequency_of_entity_vector(entry):
+
+    cleaned_content = entry["cleaned_content"]
+    lowered_cleaned_content = cleaned_content.lower()
+
+    vec = []
+
+    for talker in entry["talker"]:
+        parsed_entities = Text(talker["entity"].lower())
+
+
+        for token in parsed_entities.tokens:
+            counts[token] = get_token_count(cleaned_content, token)
+
+        avg = sum(counts.values())/len(counts.values())
+
+        vec.append(avg)
+
+    vec = np.array(vec)
+    return vec
 
 def vectorize_feature(entry):
 
     vec_1 = get_distance_of_entity_to_quote_vector(entry)
+    vec_2 = get_frequency_of_entity_vector(entry)
 
+    print(vec_2)
     vec = np.array(vec_1)
 
     return vec
