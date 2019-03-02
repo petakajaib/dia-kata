368d4638f5a0d98cc5f1d1c4366985c93126be03
Amir Othman <th3go2@gmail.com>
2019-03-02 17:53:10 +0800

    u

--- modelling.py
+++ modelling.py
@@ -22,6 +22,5 @@ for x_test_section, y_test_section in zip(x_test, y_test):
 
     y_prediction = clf.predict(x_test_section)
 
-
     print(y_prediction)
-    print(y_test_section)
+    y_test_reshaped = y_test_section.reshape(y_test_section.shape[0])
