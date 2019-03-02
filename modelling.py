d4c2a3a4bc23ade5b6dd1d1fb7d9c3acbc09f22f
Amir Othman <th3go2@gmail.com>
2019-03-02 18:02:53 +0800

    u

--- modelling.py
+++ modelling.py
@@ -4,12 +4,14 @@ import xgboost as xgb
 import numpy as np
 import pickle
 from settings import *
+
+
 vectorized_data = pickle.load(open(VECTORIZED_PATH, "rb"))
 
 x_train, x_test, y_train, y_test = train_test_split(vectorized_data["feature_vectors"], vectorized_data["target_vectors"], test_size=0.33, random_state=1337)
 
 
-clf = xgb.XGBClassifier(max_depth=8, n_jobs=6, objective="binary:logistic", random_state=1337)
+clf = xgb.XGBClassifier(max_depth=8, n_jobs=6, objective="binary:hinge", random_state=1337)
 
 x_train_stacked = np.vstack(x_train)
 y_train_stacked = np.vstack(y_train)
