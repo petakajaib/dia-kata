ff44659871899677b755acceb937e737ee375e3d
Amir Othman <th3go2@gmail.com>
2019-03-02 17:57:57 +0800

    u

--- modelling.py
+++ modelling.py
@@ -9,7 +9,7 @@ vectorized_data = pickle.load(open(VECTORIZED_PATH, "rb"))
 x_train, x_test, y_train, y_test = train_test_split(vectorized_data["feature_vectors"], vectorized_data["target_vectors"], test_size=0.33, random_state=1337)
 
 
-clf = xgb.XGBClassifier(max_depth=8, n_jobs=6, objective="binary:logistic")
+clf = xgb.XGBClassifier(max_depth=8, n_jobs=6, objective="binary:logistic", random_state=1337)
 
 x_train_stacked = np.vstack(x_train)
 y_train_stacked = np.vstack(y_train)
@@ -20,7 +20,7 @@ clf.fit(x_train_stacked, y_reshaped)
 
 for x_test_section, y_test_section in zip(x_test, y_test):
 
-    y_prediction = clf.predict(x_test_section)
+    y_prediction = clf.predict_proba(x_test_section)
 
     y_test_reshaped = y_test_section.reshape(y_test_section.shape[0])
 
