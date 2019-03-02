1dbbfe4c661c1cc5917a9647fcf47e15ff008a5d
Amir Othman <th3go2@gmail.com>
2019-03-02 17:48:22 +0800

    u

--- modelling.py
+++ modelling.py
@@ -1,9 +1,9 @@
 from sklearn.model_selection import train_test_split
+from sklearn.utils.validation import column_or_1d
 import xgboost as xgb
 import numpy as np
 import pickle
 from settings import *
-
 vectorized_data = pickle.load(open(VECTORIZED_PATH, "rb"))
 
 x_train, x_test, y_train, y_test = train_test_split(vectorized_data["feature_vectors"], vectorized_data["target_vectors"], test_size=0.33, random_state=1337)
@@ -14,7 +14,9 @@ clf = xgb.XGBClassifier(max_depth=8, n_jobs=6, objective="binary:logistic")
 x_train_stacked = np.vstack(x_train)
 y_train_stacked = np.vstack(y_train)
 
-clf.fit(x_train_stacked, y_train_stacked)
+y_reshaped = column_or_1d(y_train_stacked)
+
+clf.fit(x_train_stacked, y_reshaped)
 
 
 #
