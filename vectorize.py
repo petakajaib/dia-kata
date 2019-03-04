d58d6a5ce3af6caaad78a91f759339c7b648f7ff
Amir Othman <th3go2@gmail.com>
2019-03-04 21:30:27 +0800

    experiment

--- vectorize.py
+++ vectorize.py
@@ -37,7 +37,7 @@ def vectorize_feature(entry, fast_text_models, enriched_collection):
         get_is_organization_vector(entry, enriched_collection),
         # get_title_vector(entry, enriched_collection),
         # get_title_relative_vector(entry, enriched_collection),
-        # get_quote_vector(entry, fast_text_models, enriched_collection),
+        get_quote_vector(entry, fast_text_models, enriched_collection),
         # get_quote_relative_vector(entry, fast_text_models, enriched_collection),
         get_title_similarity_vector(entry, fast_text_models, enriched_collection)
     ]
