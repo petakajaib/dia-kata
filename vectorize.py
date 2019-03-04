395252bc86b9eff8dcaf3fa6a10e4110656e88ac
Amir Othman <amir.ot@etiqa.com.my>
2019-03-04 13:55:51 +0800

    experiment

--- vectorize.py
+++ vectorize.py
@@ -27,12 +27,12 @@ def vectorize_feature(entry, fast_text_models, enriched_collection):
         get_entity_position_vector(entry, enriched_collection),
         get_relative_frequency_ranking_vector(entry, enriched_collection),
         get_relative_entity_position_vector(entry, enriched_collection),
-        get_quote_vector(entry, fast_text_models, enriched_collection),
+        # get_quote_vector(entry, fast_text_models, enriched_collection),
         get_is_person_vector(entry, enriched_collection),
         get_is_location_vector(entry, enriched_collection),
         get_is_organization_vector(entry, enriched_collection),
-        get_title_relative_vector(entry, enriched_collection),
-        get_title_similarity_relative_vector(entry, fast_text_models, enriched_collection)
+        # get_title_relative_vector(entry, enriched_collection),
+        # get_title_similarity_relative_vector(entry, fast_text_models, enriched_collection)
     ]
 
     # for vec in get_quote_vector(entry, fast_text_models):
