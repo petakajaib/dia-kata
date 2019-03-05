e7366be8758915337f7e35d3f83e3ef6382ec5d5
Amir Othman <amir.ot@etiqa.com.my>
2019-03-05 14:20:51 +0800

    update

--- vectorization/speech_verb_vector.py
+++ vectorization/speech_verb_vector.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import numpy as np
 from polyglot.text import Text
 from settings import *
@@ -13,6 +14,11 @@ def get_speech_verb_vector(entry, enriched_collection):
     cleaned_content_entities_parsed = article["cleaned_content_entities_parsed"]
     sentences_str = [[str(token).lower()for token in sent.tokens] for sent in Text(article["cleaned_content"]).sentences]
 
+    grouped_sentences = []
+
+    for i in range(len(sentences_str)):
+        grouped_sentences.append(chain(*sentences_str[i:i+4]))
+
     vec = []
 
     for entity in entry["talker"]:
@@ -21,13 +27,13 @@ def get_speech_verb_vector(entry, enriched_collection):
         entity_tokens = cleaned_content_entities_parsed[entity_key]
         speech_verb = 0
         need_to_break = False
-        for sentence in sentences_str:
+        for sentence in grouped_sentences:
             entity_is_in_sentence = all(elem in sentence for elem in entity_tokens)
             sentence_has_speech_verb = any(elem in SPEECH_VERBS for elem in sentence)
             if entity_is_in_sentence and sentence_has_speech_verb:
                 speech_verb += 1
                 break
-                
+
         vec.append(speech_verb)
 
     return np.array(vec)
