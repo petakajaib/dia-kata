89e76dd66882bbd915762ac228e95fe2d32f17e7
Amir Othman <th3go2@gmail.com>
2019-03-13 08:25:49 +0800

    tfidf vectorize

--- clustering.py
+++ clustering.py
@@ -49,7 +49,7 @@ for label in labelled_data:
     entities = [entity["entity"] for entity in label["talker"]]
     n_grams_dictionary = Dictionary(entity_ngrams_generator(entities))
     dictionary_length = len(n_grams_dictionary)
-    print("n_grams_dictionary length", dictionary_length)
+    # print("n_grams_dictionary length", dictionary_length)
 
     model = TfidfModel(entity_bow_generator(entities, n_grams_dictionary))
 
