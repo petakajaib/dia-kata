433754435727dbc44065fc6d7eab6f153b130d9b
Amir Othman <amir.ot@etiqa.com.my>
2019-03-15 09:58:21 +0800

    filter candidates

--- inspecting_model.py
+++ inspecting_model.py
@@ -78,11 +78,11 @@ for idx, entry in enumerate(labelled_data):
     correctness = evaluate_single_extraction(predictions, target_vector_reshaped, idx, talker_entities)
 
     all_entities = [entity["entity"] for entity in entry["talker"]]
-    print("idx", idx)
-    print("quote\n", entry["quote"])
+    # print("idx", idx)
+    print("quote", entry["quote"])
     print("url", entry["source"])
 
-    print("all_entities", all_entities)
+    # print("all_entities", all_entities)
     cluster_map, inverse_cluster_map = clustering(all_entities, return_inverse=True)
     print("cluster_map", cluster_map)
     if correctness == 0:
@@ -106,10 +106,18 @@ for idx, entry in enumerate(labelled_data):
         entities_counts["correct"].append(len(all_entities))
 
         print("prediction")
-        talker_candidates = get_talker_candidates(predictions_prob, all_entities, cluster_map, inverse_cluster_map)
+        talker_candidates_prob = get_talker_candidates(predictions_prob, all_entities, cluster_map, inverse_cluster_map, return_prob)
         # pprint(talker_candidates)
+        talker_candidates = []
+        talker_prob_map = {}
+
+        for talker, prob in talker_candidates_prob:
+            talker_candidates.append(talker)
+            talker_prob_map[talker] = prob
+
         filtered_condidates = filter_candidates_by_heuristics(talker_candidates, entity_tags)
-        print(filtered_condidates)
+        print([(entity, talker_prob_map[entity])for entity in filtered_condidates
+        
         print("truth")
         pprint([entry["talker"][i]["entity"] for i, p in enumerate(target_vector_reshaped) if p==1])
 
@@ -117,9 +125,9 @@ for idx, entry in enumerate(labelled_data):
     print("---")
 
 
-print("average wrong url_count:", sum(url_counts["wrong"])/len(url_counts["wrong"]))
-print("median wrong url_count:", sorted(url_counts["wrong"])[int(len(url_counts["wrong"])/2)])
-print("max wrong url_count:", max(url_counts["wrong"]))
+# print("average wrong url_count:", sum(url_counts["wrong"])/len(url_counts["wrong"]))
+# print("median wrong url_count:", sorted(url_counts["wrong"])[int(len(url_counts["wrong"])/2)])
+# print("max wrong url_count:", max(url_counts["wrong"]))
 
 print("average correct url_count:", sum(url_counts["correct"])/len(url_counts["correct"]))
 print("median correct url_count:", sorted(url_counts["correct"])[int(len(url_counts["correct"])/2)])
@@ -129,6 +137,6 @@ print("avg entities count for correct predictions", sum(entities_counts["correct
 print("median entities count for correct predictions", sorted(entities_counts["correct"])[int(len(entities_counts["correct"])/2)])
 print("max entities count for correct predictions", max(entities_counts["correct"]))
 
-print("avg entities count for wrong predictions", sum(entities_counts["wrong"])/len(entities_counts["wrong"]))
-print("median entities count for wrong predictions", sorted(entities_counts["wrong"])[int(len(entities_counts["wrong"])/2)])
-print("max entities count for wrong predictions", max(entities_counts["wrong"]))
+# print("avg entities count for wrong predictions", sum(entities_counts["wrong"])/len(entities_counts["wrong"]))
+# print("median entities count for wrong predictions", sorted(entities_counts["wrong"])[int(len(entities_counts["wrong"])/2)])
+# print("max entities count for wrong predictions", max(entities_counts["wrong"]))
--- talker_candidate.py
+++ talker_candidate.py
@@ -10,15 +10,23 @@ def get_talker_candidates(predictions_prob, entities, cluster_map, inverse_clust
         if prediction_prob[1] < 0.5:
             continue
 
-        if cluster_map[entity] > -1:
+        if cluster_map[entity] != -1:
 
             predictions_set = predictions_set.union(inverse_cluster_map[cluster_map[entity]])
-
-            for ent in inverse_cluster_map[cluster_map[entity]]:
-                entity_prob_map[ent] = prediction_prob
+            if return_prob:
+                for ent in inverse_cluster_map[cluster_map[entity]]:
+                    entity_prob_map[ent] = prediction_prob
         else:
             predictions_set.add(entity)
-            entity_prob_map[entity] = prediction_prob
+
+    if return_prob:
+        for prediction_prob, entity in zip(predictions_prob,  entities):
+
+            if prediction_prob[1] < 0.5:
+                continue
+
+            if cluster_map[entity] == -1:
+                entity_prob_map[entity] = prediction_prob
 
     if return_prob:
         return [(p, entity_prob_map[p]) for p in predictions_set]
