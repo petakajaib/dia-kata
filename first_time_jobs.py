df83d7696ca4ce08039cc9a6548dc301386f94c6
Amir Othman <petakajaib@gmail.com>
2019-04-06 13:53:53 +0800

    entity fasttext

--- first_time_jobs.py
+++ first_time_jobs.py
@@ -1,9 +1,10 @@
-from pymongo import MongoClient
+from datetime import datetime
+from annoy import AnnoyIndex
 from gensim.models.fasttext import FastText
 from polyglot.text import Text
-from settings import *
-from datetime import datetime
 import pycld2
+from pymongo import MongoClient
+from settings import *
 
 def entity_generator(collection):
     for article in collection.find({"entities": {"$exists": True}}):
@@ -40,42 +41,74 @@ def populate_entity_collection(article_collection, entity_collection):
                 print(err)
                 continue
 
+def build_fast_text_model():
+    # build fastText
+
+    fasttext_params = {
+        "hs": 1,
+        "window": 10,
+        "min_count": 1,
+        "workers": 7,
+        "min_n": 1,
+        "max_n": 10,
+    }
+
+    print("building corpus")
+
+    entity_corpus = [entity for entity in entity_generator(entity_collection)]
+    fasttext_entity = FastText(**fasttext_params)
+
+    print("count corpus")
+    fasttext_entity.build_vocab(sentences=entity_corpus)
+    total_examples = fasttext_entity.corpus_count
+
+    print("train fasttext")
+    fasttext_entity.train(sentences=entity_corpus, total_examples=total_examples, epochs=5)
+
+    print("saving fasttext")
+
+    fasttext_entity.save(FASTTEXT_ENTITY)
+
+    return fasttext_entity
+
 client = MongoClient()
 
 db = client[MONGO_DB]
 article_collection = db[MONGO_COLLECTION]
 entity_collection = db[ENTITY_COLLECTION]
+annoy_index_collection = db[ANNOY_INDEX_COLLECTION]
 
 # populate_entity_collection(article_collection, entity_collection)
 
-# For querying
-
-# build fastText
-
-fasttext_params = {
-    "hs": 1,
-    "window": 10,
-    "min_count": 1,
-    "workers": 7,
-    "min_n": 1,
-    "max_n": 10,
-}
-
-print("building corpus")
-
-entity_corpus = [entity for entity in entity_generator(entity_collection)]
-fasttext_entity = FastText(**fasttext_params)
-
-print("count corpus")
-fasttext_entity.build_vocab(sentences=entity_corpus)
-total_examples = fasttext_entity.corpus_count
-
-print("train fasttext")
-fasttext_entity.train(sentences=entity_corpus, total_examples=total_examples, epochs=5)
-
-print("saving fasttext")
-
-fasttext_entity.save(FASTTEXT_ENTITY)
+# build_fast_text_model()
+fasttext_entity = FastText.load(FASTTEXT_ENTITY)
 
 # build annoyIndex
 #
+dimension = 100
+annoy_index = AnnoyIndex(dimension)
+
+idx = 1
+annoy_index_collection.delete_many({})
+
+for entities in entity_generator(collection):
+
+    for entity in entities:
+
+        if annoy_index_collection.count({"entity": entity}) == 0:
+
+            print(entity)
+
+            annoy_index_collection.insert_one({
+                "idx": idx,
+                "entity": entity
+                })
+
+            vector = fasttext_entity[entity]
+
+            annoy_index.add_item(idx, vector)
+
+            idx += 1
+
+annoy_index.build(10)
+annoy_index.save(ANNOY_INDEX_PATH)
