921d9dc9d0c94d959cbe314f9715f16c7587c23f
Amir Othman <petakajaib@gmail.com>
2019-04-06 13:34:42 +0800

    entity fasttext

--- first_time_jobs.py
+++ first_time_jobs.py
@@ -9,42 +9,44 @@ def entity_generator(collection):
     for article in collection.find({"entities": {"$exists": True}}):
         yield article["entities"]
 
+def populate_entity_collection(article_collection, entity_collection):
+    query = {
+        "publish_date": {"$gte": datetime(2019,4,5)},
+        "content": {"$exists": True}
+    }
+
+    for article in article_collection.find(query):
+
+        print(article["url"])
+
+        if entity_collection.count({"url": article["url"]}) == 0:
+
+            try:
+                parsed = Text(article["content"])
+                entities = [" ".join(entity) for entity in parsed.entities]
+
+                entity = {
+                    "entities": entities,
+                    "url": article["url"],
+                    "detected_language": parsed.detect_language(),
+                    "publish_date": article["publish_date"]
+                }
+
+                entity_collection.insert_one(entity)
+
+            except pycld2.error as err:
+                print(err)
+            except ValueError as err:
+                print(err)
+                continue
+
 client = MongoClient()
 
 db = client[MONGO_DB]
 article_collection = db[MONGO_COLLECTION]
 entity_collection = db[ENTITY_COLLECTION]
 
-query = {
-    "publish_date": {"$gte": datetime(2019,4,5)},
-    "content": {"$exists": True}
-}
-
-for article in article_collection.find(query):
-
-    print(article["url"])
-
-    if entity_collection.count({"url": article["url"]}) == 0:
-
-        try:
-            parsed = Text(article["content"])
-            entities = [" ".join(entity) for entity in parsed.entities]
-
-            entity = {
-                "entities": entities,
-                "url": article["url"],
-                "detected_language": parsed.detect_language(),
-                "publish_date": article["publish_date"]
-            }
-
-            entity_collection.insert_one(entity)
-
-        except pycld2.error as err:
-            print(err)
-        except ValueError as err:
-            print(err)
-            continue
-
+# populate_entity_collection(article_collection, entity_collection)
 
 # For querying
 
@@ -73,7 +75,7 @@ fasttext_entity.train(sentences=entity_corpus, total_examples=total_examples, ep
 
 print("saving fasttext")
 
-fasttext_entity.save("../fastTextEntity.bin")
+fasttext_entity.save(FASTTEXT_ENTITY)
 
 # build annoyIndex
 #
