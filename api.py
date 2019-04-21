fa589b62fb32d35b758e2b74b5ecdd826369fa69
Amir Othman <petakajaib@gmail.com>
2019-04-21 10:29:40 +0800

    api

--- api.py
+++ api.py
@@ -1,19 +1,48 @@
-from flask import Flask, jsonify
-from random import random
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from gensim.models.fasttext import FastText
+from annoy import AnnoyIndex
+from first_time_jobs import search_entities
+from settings import *
 
 api = Flask(__name__)
 
-d = {"bo": "la"}
+dimension = 100
 
-@api.route("/search/")
+client = MongoClient()
+
+db = client[MONGO_DB]
+article_collection = db[MONGO_COLLECTION]
+entity_collection = db[ENTITY_COLLECTION]
+annoy_index_collection = db[ANNOY_INDEX_COLLECTION]
+quote_collection = db[QUOTE_COLLECTION]
+enriched_collection = db[MONGO_COLLECTION_ENRICHED]
+similar_entities_collection = db[SIMILAR_ENTITIES_COLLECTION]
+entity_keywords_collection = db[ENTITY_KEYWORDS_COLLECTION]
+
+annoy_index = AnnoyIndex(dimension)
+annoy_index.load(ANNOY_INDEX_PATH)
+fasttext_entity = FastText.load(FASTTEXT_ENTITY)
+
+@api.route("/search/", methods=['POST'])
 def search():
 
+    request_body = request.get_json()
+
+    print(request_body)
+
+    
     searched = []
     indices = list(annoy_index.get_nns_by_vector(vector, 20))
     results = [entry["entity"] for entry in annoy_index_collection.find({"idx": {"$in": indices}})]
 
     print("search")
     print(results)
+
+    search_entities(
+        query, fasttext_entity,
+        annoy_index, annoy_index_collection,
+        n_results=20)
 
 
     return jsonify(**d)
@@ -28,9 +57,9 @@ def top_people():
     return jsonify(top_people=["person1", "person2", "person3"])
 
 @api.route("/fasttext/")
-def load_models():
+def reload_fasttext_models():
     return jsonify(ja=['pa'])
 
 @api.route("/annoy/")
-def load_models():
+def reload_annoy_index():
     return jsonify(ja=['pa'])
--- first_time_jobs.py
+++ first_time_jobs.py
@@ -156,6 +156,25 @@ def get_similar_entities(
 
     return aggregated[:n_results]
 
+def search_entities(
+    query, fasttext_entity,
+    annoy_index, annoy_index_collection,
+    n_results=20):
+
+    vector = fasttext_entity[query]
+
+    aggregated = []
+    for result in annoy_index.get_nns_by_vector(vector, n_results):
+
+        res = annoy_index_collection.find_one({"idx": result})
+
+        query_set = set(query.lower().split())
+        entity_set = set(res["entity"].split())
+
+        aggregated.append(res["entity"])
+
+    return aggregated[:n_results]
+
 def batch_quote_extraction(
         quote_model_path,
         article_collection,
