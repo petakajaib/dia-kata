a1b49725a8e94a51877ac6329e6cc0f5ba4a1ff0
Amir Othman <petakajaib@gmail.com>
2019-05-24 05:52:52 +0800

    write script to sample production database

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from annoy import AnnoyIndex
 from flask_cors import CORS
+from gensim.models.fasttext import FastText
 from logging.handlers import RotatingFileHandler
 from flask_config import app_config
+from settings import (
+    ANNOY_INDEX_PATH,
+    FASTTEXT_ENTITY
+)
 from . import routes
 
 
 def create_app(environment):
+
+    dimension = 100
+    annoy_index = AnnoyIndex(dimension)
+    annoy_index.load(ANNOY_INDEX_PATH)
+    fasttext_entity = FastText.load(FASTTEXT_ENTITY)
+
     app = Flask(__name__, instance_relative_config=True)
     handler = RotatingFileHandler(
         "logs/api.log",
@@ -14,5 +26,6 @@ def create_app(environment):
     app.config.from_object(app_config[environment])
     app.logger.addHandler(handler)
     CORS(app)
-    routes.init_app(app)
+    routes.init_app(app, annoy_index, fasttext_entity)
+
     return app
--- api/routes.py
+++ api/routes.py
@@ -1,26 +1,13 @@
 from flask import request, Response, jsonify, render_template
-from gensim.models.fasttext import FastText
-from annoy import AnnoyIndex
 from .mongo_collections import (
     annoy_index_collection,
     entity_keywords_collection,
     quote_collection
 )
 from .controllers import get_detail, get_search_results
-from settings import (
-    ANNOY_INDEX_PATH,
-    FASTTEXT_ENTITY
-)
 
 
-dimension = 100
-
-annoy_index = AnnoyIndex(dimension)
-annoy_index.load(ANNOY_INDEX_PATH)
-fasttext_entity = FastText.load(FASTTEXT_ENTITY)
-
-
-def init_app(app):
+def init_app(app, annoy_index, fasttext_entity):
 
     @app.route("/", methods=['GET'])
     def front_page():
