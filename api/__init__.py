be6f06e7c7e2933d83e05d516f43ee2f33999689
Amir Othman <th3go2@gmail.com>
2019-05-26 11:18:25 +0800

    set streamhandler logging

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from annoy import AnnoyIndex
 from flask_cors import CORS
 from gensim.models.fasttext import FastText
-from logging.handlers import RotatingFileHandler
+import logging
+from logging.handlers import RotatingFileHandler, StreamHandler
 from flask_config import app_config
 from settings import (
     ANNOY_INDEX_PATH,
@@ -23,8 +24,11 @@ def create_app(environment):
         "logs/api.log",
         maxBytes=512*1024*1024,
         backupCount=10)
+    stream_handler = StreamHandler()
+    stream_handler.set_level(logging.DEBUG)
     app.config.from_object(app_config[environment])
     app.logger.addHandler(handler)
+    app.logger.addHandler(stream_handler)
     CORS(app)
     routes.init_app(app, annoy_index, fasttext_entity)
 
